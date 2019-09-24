1cda276d00f95d9778b59c183f3559a62478e703
jordanjenner <jordan.jenner12@gmail.com>
2019-09-24 21:08:03 +0100

    Added social links to index context.

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Page
+from .models import Page, SocialLink
 
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
     pages = Page.objects.order_by('position')
+    socials = SocialLink.objects.all()
     print(pages)
 
     context = {
-        "pages":    pages
+        "pages":    pages,
+        "socials":  socials,
     }
     return render(request, "index.html", context)
