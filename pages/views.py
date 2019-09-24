f9e08f917197cbd023c42f4814920af78d15f749
jordanjenner <jordan.jenner12@gmail.com>
2019-09-24 20:30:44 +0100

    Added pages app.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    pages = Page.objects.order_by('position')
+    print(pages)
+
+    context = {
+        "pages":    pages
+    }
+    return render(request, "index.html", context)
