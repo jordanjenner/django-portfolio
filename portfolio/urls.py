302ffa2320bd29d48fe79753fae36c9102e70fa1
jordanjenner <jordan.jenner12@gmail.com>
2019-09-27 17:06:09 +0100

    Added about url.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,5 +20,6 @@ from pages import views
 urlpatterns = [
     path('', views.home_view),
     path('projects/', views.projects_view),
+    path('about/', views.about_view),
     path('admin/', admin.site.urls),
 ]
