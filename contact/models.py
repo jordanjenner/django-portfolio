8af313c4c2dbea7382d361f58177daa0c17b540f
jordanjenner <jordan.jenner12@gmail.com>
2019-09-30 17:48:41 +0100

    Added Message model to store messages in DB.

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    email = models.EmailField()
+    message = models.TextField()
+    date_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        date = self.date_time.strftime("%Y-%m-%d %H:%M:%S")
+        return "Message from {} at {}".format(self.email, date)
