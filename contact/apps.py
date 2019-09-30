5247e60b3dd03870d4ddf1def32d3f04ed0c1958
jordanjenner <jordan.jenner12@gmail.com>
2019-09-30 17:48:15 +0100

    Added a send email app.

--- contact/apps.py
+++ contact/apps.py
@@ -1,5 +1,27 @@
 from django.apps import AppConfig
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class ContactConfig(AppConfig):
     name = 'contact'
+
+
+def send_email(email_address, message_body, date_time):
+    date = date_time.strftime("%Y-%m-%d %H:%M:%S")
+    message = """You recieved a message from {} at {}.
+    
+    \"{}\"
+    
+    This message was sent from the contact form on jordanjenner.com
+    """.format(email_address, date, message_body)
+
+
+    send_mail(
+        'New email from contact form @ jordanjenner.com',
+        message,
+        email_address,
+        [settings.CONTACT_EMAIL],
+        fail_silently=False
+    )
+
