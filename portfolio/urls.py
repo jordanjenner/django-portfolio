ee8bd5b02f09a3516a8443c6c84fdced237e267f
jordanjenner <jordan.jenner12@gmail.com>
2020-11-03 13:31:41 +0000

    Added favicon

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path
 from pages import views
 
 urlpatterns = [
     path('', views.home_view),
+    path('favicon.ico', RedirectView.as_view(url='static/favicon.ico', permanent=True), name='favicon'),
     path('admin/', admin.site.urls),
 ]
